78375db50f2343fdb734c3b92457f2a1ebb337c8
Rulo <andres@rulocode.com>
2022-03-10 06:39:50 -0500

    feat(arquitecture): init commit

---
+++ src/components/atoms/button/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Button = () => {
+  return (
+    <button>hola click here</button>
+  )
+}
+
+export default Button
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import {Button} from '../components/atoms/button/src/index'
+
+const Home: NextPage = () => {
+  return (
+    <Button />
+  )
+}
+
+export default Home
