e5b65243ce741249942d7d9fdbf79982c02a27a4
Rulo <andres@rulocode.com>
2022-03-11 06:31:00 -0500

    feat(linters): adding pre-push

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../../styles/globals.css';
+import React from 'react'
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles, lightTheme } from '../../styles/globalStyles';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import type { NextPage } from 'next';
 import { ButtonComponent } from '../components/atoms/button/src';
 
